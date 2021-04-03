a307dd3f43345a39f867a482f3abd392cd38498e
gavin <2522687049@qq.com>
2021-04-04 02:51:42 +0800

    作业week03

---
+++ src/main/java/com/week03/tm1/HttpClientTest.java
@@ -0,0 +1,27 @@
+package com.week03.tm1;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import sun.net.www.http.HttpClient;
+
+/**
+ * @author Gavin
+ */
+public class HttpClientTest extends HttpClient {
+
+    public static void main(String[] args) throws Exception {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet httpGet = new HttpGet("http://localhost:8808/test");
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+        HttpEntity responseEntity = response.getEntity();
+        System.out.println("响应状态为:" + response.getStatusLine());
+        if (responseEntity != null) {
+            System.out.println("响应内容长度为:" + responseEntity.getContentLength());
+            System.out.println("响应内容为:" + EntityUtils.toString(responseEntity));
+        }
+    }
+}
